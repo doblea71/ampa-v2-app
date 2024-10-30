3adaedb0780928dfcdc5121052d93009bf2ada37
Angel E. Antunez V. <doble.a71@gmail.com>
2024-10-30 13:40:31 -0400

    Acomodando el footer a la parte mas baja, pegado al bottom de todas las paginas y mejorando sus estilos.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
 <!-- src/components/Footer.svelte -->
-<footer class="bg-gray-800 text-white text-sm font-bold text-center mt-4 p-4">
+<footer class="bg-gray-800 text-white text-sm font-bold text-center p-2">
 	<p>© 2024 AMPA/AFAM. Todos los derechos reservados.</p>
 </footer>
